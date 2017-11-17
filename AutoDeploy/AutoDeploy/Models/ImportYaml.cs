6aa426a3f4bccd781e92aa1934fb25f6a9790d2e
infinith4 <infinith4@gmail.com>
2017-11-18 06:12:31 +0900

    read yaml

---
+++ AutoDeploy/AutoDeploy/Models/ImportYaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+
+namespace AutoDeploy.Models
+{
+    public class ImportYaml
+    {
+        public class DeserializedObject
+        {
+            [YamlMember(Alias = "originalpath")]
+            public string originalpath { get; set; }
+
+            [YamlMember(Alias = "servers")]
+            public List<Server> servers { get; set; }
+
+            public class Server
+            {
+                public string path { get; set; }
+            }
+        }
+    }
+}
--- AutoDeploy/AutoDeploy/Program.cs
+++ AutoDeploy/AutoDeploy/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YamlDotNet.RepresentationModel;
 
 namespace AutoDeploy
 {
@@ -11,13 +12,16 @@ namespace AutoDeploy
     {
         static void Main(string[] args)
         {
-            string orgPath = @"C:\Gitrepo\AutoDeploy\testdir\";
+            var yamlImporter = new Utils.YamlUtils.YamlImporter();
+            Models.ImportYaml.DeserializedObject obj = yamlImporter.Deserialize("config.yaml");
+
+            string orgPath = obj.originalpath;
             IEnumerable<string> dirs =
                 Directory.EnumerateDirectories(
-                    orgPath, "*", System.IO.SearchOption.AllDirectories);
+                    orgPath, "*", SearchOption.AllDirectories);
             IEnumerable<string> files =
                 Directory.EnumerateFiles(
-                    orgPath, "*", System.IO.SearchOption.AllDirectories);
+                    orgPath, "*", SearchOption.AllDirectories);
 
             //ファイルを列挙する
             foreach (string f in files)
@@ -25,24 +29,26 @@ namespace AutoDeploy
                 Console.WriteLine(f);
             }
 
-            //To Directory
-            string dest_directory = @"C:\Gitrepo\AutoDeploy\cp_testdir";
-
-            foreach (string dir in dirs)
+            foreach (var item in obj.servers)
             {
-                string dest_dir = string.Format(@"{0}\{1}", dest_directory, dir.Replace(orgPath, string.Empty));
+                //To Directory
+                string dest_directory = item.path;
 
-                if (!Directory.Exists(dest_dir))
+                foreach (string dir in dirs)
                 {
-                    Directory.CreateDirectory(dest_dir);
+                    string dest_dir = string.Format(@"{0}\{1}", dest_directory, dir.Replace(orgPath, string.Empty));
+
+                    if (!Directory.Exists(dest_dir))
+                    {
+                        Directory.CreateDirectory(dest_dir);
+                    }
+                }
+                foreach (string file in files)
+                {
+                    string dest_file = string.Format(@"{0}\{1}", dest_directory, file.Replace(orgPath, string.Empty));
+                    File.Copy(file, dest_file, true);
                 }
             }
-            foreach (string file in files)
-            {
-                string dest_file = string.Format(@"{0}\{1}", dest_directory, file.Replace(orgPath, string.Empty));
-                File.Copy(file, dest_file, true);
-            }
-            Console.WriteLine("");
         }
     }
 }
